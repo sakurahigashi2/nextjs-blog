c40bfaa5f8132b2248ec83617f08fc6e2b38b4fd
sakurahigashi2 <joh.murata@gmail.com>
2021-07-05 00:19:18 +0900

    Finish to Pre-rendering and Data Fetching

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
+import { getSortedPostsData } from '../lib/posts'
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.scss'
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -17,6 +27,21 @@ export default function Home() {
             <a>To post</a>
           </Link>
         </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   )
